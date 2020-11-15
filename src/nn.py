33ef13fe96f87326625575f0516f9a973396a442
Marcus Pernow <marcus.pernow@gmail.com>
2020-11-15 17:28:52 +0100

    implemented backprop

--- src/nn.py
+++ src/nn.py
@@ -40,9 +40,62 @@ class NN:
             a = sigmoid(np.dot(w, a) + b)
         return a
 
+    def backprop(self, x, y):
+        """
+        Implements back propagation
+        Returns a tuple of gradients of cost function w.r.t each parameter,
+        (nabla_b, nabla_w), which are of the same shape as self.biases,
+        self.weights.
+        Input: x is input to the network, y is correct output.
+        """
+        # lists to store derivatives of cost function w.r.t each weight and bias
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # feed forward and store all activations:
+        activation = x
+        activations = [x] # store the sigmoid(z) variables
+        zs = [] # store the w.x+b variables
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activations) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        
+        # go backward and store results in nabla_{b,w}
+        delta = self.cost_prime(activations[-1], y) * sigmoid_prime(zs[-1])
+        # derivative w.r.t last layer params
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # loop through the rest of the network
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            delta = np.dot(self.weights[-l+1].transpose(), delta) *\
+            sigmoid_prime(z)
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
+        return (nabla_b, nabla_w)
+
+        
+
+    def cost_prime(self, pred, true):
+        """
+        Derivative of cost function
+        Assumes distance squared between predicted and true values as the cost
+        function.
+        """
+        return (pred - true)
+
 
 def sigmoid(z):
     """
     Sigmoid function of variable z
     """
     return 1./(1. + np.exp(-z))
+
+def sigmoid_prime(z):
+    """
+    Derivative of sigmoid
+    """
+    return sigmoid(z) * (1. - sigmoid(z))
